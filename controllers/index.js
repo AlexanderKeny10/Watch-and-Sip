4ad2f0e89d9fb3b9ad980b954ca52bfc88e4268b
Britt Ralph <brittney.mr22@gmail.com>
2020-11-16 12:08:37 -0500

    modified

--- controllers/index.js
+++ controllers/index.js
@@ -1,23 +1,12 @@
 const router = require('express').Router();
 
 const apiRoutes = require('./api/');
-<<<<<<< HEAD
-const homeRoutes = require('./home-routes.js');
-const dashboardRoutes = require('./dashboard-routes.js');
-const trendingRoutes = require('./trending-routes.js');
-
-router.use('/', homeRoutes);
-router.use('/api', apiRoutes);
-router.use('/dashboard', dashboardRoutes);
-router.use('/trending', trendingRoutes);
-=======
 const searchRoutes = require('./search-routes.js');
 const viewRoutes = require('./view-routes.js');
 
 router.use('/search', searchRoutes); // calls moviedb API
 router.use('/api', apiRoutes); // post/get users, post/get reviews
 router.use('/dashboard', viewRoutes); // routes to handlebars views
->>>>>>> develop
 
 
 router.use((req, res) => {
