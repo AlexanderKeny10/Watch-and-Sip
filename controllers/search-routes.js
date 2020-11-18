7cce53594ab46ffbf8f49fad745a2aff5c08c671
AKen00 <ajken04@gmail.com>
2020-11-17 21:04:22 -0500

    made a path to from end for movies

--- controllers/search-routes.js
+++ controllers/search-routes.js
@@ -7,17 +7,19 @@ router.get('/movie/:movie', (req, res) => {
     url: `https://api.themoviedb.org/3/search/movie?api_key=5a6e2163790bbd3d160e06904e80d572&language=en-US&query=${userInput}&page=1&include_adult=false`,
     method: 'get',
   })
+    // 
     .then(response => {
       const movieResults = response.data.results
       // res.json(response.data);
       console.log(response.data.results)
       res.render('movie-search', {
         layout: 'main',
-        movieResults})
+        movieResults
+      })
     });
 });
 
-router.get('/tvshow', (req, res) => {
+router.get('/tvshow/:tvshow', (req, res) => {
   const userInput = req.params.tvshow; //query needs tvshow key
   axios({
     url: `https://api.themoviedb.org/3/search/tv?api_key=5a6e2163790bbd3d160e06904e80d572&language=en-US&query=${userInput}&page=1&include_adult=false`,
@@ -26,9 +28,10 @@ router.get('/tvshow', (req, res) => {
     .then(response => {
       const tvResults = response.data.results
       console.log(tvResults)
-      res.render('tv-search',{
+      res.render('tv-search', {
         layout: 'main',
-        tvResults}) 
+        tvResults
+      })
     });
 });
 
--- public/javascript/search-movie.js
+++ public/javascript/search-movie.js
@@ -1,21 +1,29 @@
 
-const movieFormHandler =  async function(event) {
+const movieFormHandler = async function (event) {
     event.preventDefault();
 
-// grab user input from search box send a query
-const movieTitle = document.querySelector('input[name="movieTitle"]').value;
+    // grab user input from search box send a query
+    const movieTitle = document.querySelector('input[name="movieTitle"]').value;
 
-// make api call from front-end to back-end
-fetch(`/search/movie/${movieTitle}`, {
-method: "get",
-headers: { 'Content-Type': 'application/json' },
-})
-// console.log(movieTitle)
-    .then(function() {
-        document.location.replace('movie-search');
-        // document.location.reload();
-        console.log("working btn")
-    });
+    const route = `/search/movie/${movieTitle}`;
+    location.replace(route);
+    // make api call from front-end to back-end
+    // fetch(`/search/movie/${movieTitle}`, {
+    //     method: "get",
+    //     headers: { 'Content-Type': 'application/json' },
+    // })
+    //     // console.log(movieTitle)
+    //     .then(function (response) {
+    //         // add data 
+    //         // document.location.replace('movie-search');
+    //         // document.location.reload();
+    //         // console.log("working btn")
+    //         return response.text()
+    //     })
+    //     .then(function(data) {
+    //         console.log(data)
+    //     })
+
 
 }
 // remember this pattern
