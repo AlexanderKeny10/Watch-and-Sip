17a1601e11eecbf27a15732870402db58316dfe0
Britt Ralph <brittney.mr22@gmail.com>
2020-11-18 13:05:00 -0500

    modified

--- seeds/seed-all.js
+++ seeds/seed-all.js
@@ -6,10 +6,10 @@ const sequelize = require('../config/connection');
 const seedAll = async () => {
   await sequelize.sync({ force: true });
   console.log('\n----- DATABASE SYNCED -----\n');
-  await seedReviews();
-  console.log('\n----- REVIEWS SEEDED -----\n');
   await seedUsers();
   console.log('\n----- USERS SEEDED -----\n');
+  await seedReviews();
+  console.log('\n----- REVIEWS SEEDED -----\n');
   
   
   process.exit(0);
