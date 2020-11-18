6502ed2392d9db77b8392701066d751b76f94760
Britt Ralph <brittney.mr22@gmail.com>
2020-11-18 18:23:50 -0500

    modified

--- public/javascript/cocktails.js
+++ public/javascript/cocktails.js
@@ -1,12 +1,12 @@
 
-const drinkFormHandler = async function (event) {
-    event.preventDefault();
+// const drinkFormHandler = async function (event) {
+//     event.preventDefault();
 
-    // grab user input from search box send a query
-    const newDrink = document.querySelector('input[name="newDrink"]').value;
+//     // grab user input from search box send a query
+//     const newDrink = document.querySelector('input[name="newDrink"]').value;
 
-    const route = `/search/drink/${newDrink}`;
-    location.replace(route);
-}
-// remember this pattern
-document.querySelector('#drink-form').addEventListener('submit', drinkFormHandler);
+//     const route = `/search/drink/${newDrink}`;
+//     location.replace(route);
+// }
+// // remember this pattern
+// document.querySelector('#drink-form').addEventListener('submit', drinkFormHandler);
