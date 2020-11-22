05afa14f10146df32f3596232291d89487ecb605
Champion113 <sbalbright19@gmail.com>
2020-11-22 16:20:28 -0500

    modified login handelbars

--- seeds/seed-all.js
+++ seeds/seed-all.js
@@ -3,30 +3,16 @@ const seedUsers = require('./user-seeds');
 
 const sequelize = require('../config/connection');
 
-<<<<<<< HEAD
-const seedAll = async () => {
-  await sequelize.sync({ force: true });
-  console.log('\n----- DATABASE SYNCED -----\n');
-  await seedUsers();
-  console.log('\n----- USERS SEEDED -----\n');
-  await seedReviews();
-  console.log('\n----- REVIEWS SEEDED -----\n');
-  
-  
-  process.exit(0);
-};
-=======
 const seedAll = async() => {
     await sequelize.sync({
         force: true
     })
     console.log("database synced")
-    const response = await userSeeds();
+    const response = await seedUsers();
     console.log('users seeded', response)
-    await reviewSeeds();
+    await seedReviews();
     console.log('reviews seeded')
     process.exit(0)
 }
->>>>>>> 82dd437f5215b8575234dc36140e62d3c89acf21
 
 seedAll();
